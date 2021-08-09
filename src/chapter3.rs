c6d4e5587cae4390e7565f7ff440dadc53715d42
riki.okunishi <riki.okunishi@nislab.doshisha.ac.jp>
2021-08-09 18:31:36 +0900

    Proceed chapter 3.3

--- src/chapter3.rs
+++ src/chapter3.rs
@@ -1,6 +1,7 @@
 
 mod chapter3_1;
 mod chapter3_2;
+mod chapter3_3;
 
 pub fn chapter3() {
 
@@ -11,4 +12,7 @@ pub fn chapter3() {
 
     println!("\n*** Chapter3.2 ***");
     chapter3_2::chapter3_2();
+
+    println!("\n*** Chapter3.3 ***");
+    chapter3_3::chapter3_3();
 }
---
+++ src/chapter3/chapter3_3.rs
@@ -0,0 +1,50 @@
+
+// see https://doc.rust-jp.rs/book-ja/ch03-03-how-functions-work.html
+
+pub fn chapter3_3() {
+    one_function();
+    what_is_function();
+}
+
+// Rustでは変数や関数の命名はスネークケース．関数の宣言順序は無関係．
+fn one_function() {
+    println!("Hello, world!");
+
+    another_function(5);
+}
+
+// 引数は「仮引数: 型」の形．型は宣言しなければならないようになっている．
+fn another_function(x: i32) {
+    println!("The value of x is {}.", x);
+}
+
+// 関数は「全部文」または「文が続いた最後に式」で構成．Rustは式が文の一部になる違いがある「式指向言語」．
+// 文は，何らかの動作をして値を返さない．
+// 式は．結果値として評価される．
+fn what_is_function() {
+    let y = 6; // let文．文なので終端はセミコロン(;)
+    // let x = (let y = 6); // 文は値を返さないので，これはコンパイルエラー．C言語系統のようなことはできない．
+    // let x = y = 8; // なのでこれもダメ．
+    let z = 5 + 6; // 「5 + 6」は式．
+    another_function(4); // 関数呼び出しは式．
+    println!("It is equation."); // マクロ呼び出しも式．
+    let y = {
+        let x = 6; // ココは文だが，
+        x + 5 // ココが式なので，ブロックが式を返し，全体として式．
+    }; // 新しいスコープを作る {} も式．
+
+}
+
+// 関数の戻り値は '->' に続いて型を明記する．
+fn five() -> i32 {
+    5
+}
+
+// 関数の戻り値を宣言されていないとコンパイルエラー．
+/*fn one() {
+    1
+}*/
+
+fn ten() -> i32 {
+    return 10 // return を使えば明示的．文末にセミコロンをつけてもOK．
+}
